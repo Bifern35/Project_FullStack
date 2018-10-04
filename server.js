df70a3a0353785a56ba7613f50269f8f5d4b03d1
Bifern35 <s5930213005@phuket.psu.ac.th>
2018-10-04 16:39:24 +0700

    add form new product

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ app.get('/products/:pid', function(req,res){
 app.post('/product/update',function(req, res) {
     var id = req.body.id;
     var title = req.body.title;
-    var sql=`update product set title = ${title}, price= ${price} = where id = ${id}`;
+    var price = req.body.price;
+    var sql=`update products set title = ${title}, price= ${price} = where id = ${id}`;
     console.log('Update: '+sql);
     res.redirect('/products');
 
