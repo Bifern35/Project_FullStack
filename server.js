b2232ad602b2935435d52a8c842c6609b7b7b490
Bifern35 <s5930213005@phuket.psu.ac.th>
2018-10-05 12:57:33 +0700

    test edit product3

--- server.js
+++ server.js
@@ -54,6 +54,7 @@ app.get('/products/:pid', function(req,res){
 app.post('/product/update',function(req, res) {
     var id = req.body.id;
     var title = req.body.title;
+    var price = req.body.price;
     var sql=`update product set title = ${title}, price= ${price} = where id = ${id}`;
     console.log('Update: '+sql);
     res.redirect('/products');
