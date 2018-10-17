c9149a0d6460a6a9e1f540d99e5329429e422d7f
Bifern35 <s5930213005@phuket.psu.ac.th>
2018-10-17 17:29:15 +0700

    change bg color

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static( "public" ));
 
 app.set('view engine','ejs');
 app.get('/', function(req,res){
@@ -117,6 +118,7 @@ app.get('/purchases', function(req,res){
         })
     
 });
+
 var port = process.env.PORT || 3000;
     app.listen(port, function() {
     console.log('App is running on http://localhost:' + port);
